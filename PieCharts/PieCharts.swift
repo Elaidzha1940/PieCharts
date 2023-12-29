287de67cc659e4580aa7081ed9246260d012b9ba
Elaidzha1940 <eli080293@gmail.com>
2023-12-29 18:39:41 +0300

    PieCharts

---
+++ PieCharts/PieCharts.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: PieCharts
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct PieCharts: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    PieCharts()
+}
---
+++ PieCharts/PieChartsApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: PieCharts
+//  File: PieChartsApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PieChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PieCharts()
+        }
+    }
+}
